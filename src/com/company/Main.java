ac239da78308624391bdbe86c436696b0ca0f5ad
zoubeidaNM <zoumenz@yahoo.com>
2017-10-11 11:54:58 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("+--------------------------------------+");
+        System.out.println("|      Java Bank ATM Receipt           |");
+        System.out.println("|      Wednesday, December 2, 2015     |");
+        System.out.println("|      ATM Location # 123              |");
+        System.out.println("|                                      |");
+        System.out.println("|                                      |");
+        System.out.println("|      Account Number:      1234567    |");
+        System.out.println("|      Customer:     John Q. Public    |");
+        System.out.println("|      Transaction Type:    Deposit    |");
+        System.out.println("|      Transaction Amount:  $500.00    |");
+        System.out.println("|      Account Balance:   $1,500.00    |");
+        System.out.println("|                                      |");
+        System.out.println("|      Thank you for banking with us   |");
+        System.out.println("|            Have a coffee day         |");
+        System.out.println("|                                      |");
+        System.out.println("+--------------------------------------+");
+    }
+}
